d8cad75277e62ac0cdd5dff60391f7a3c57caae4
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-09-19 20:48:43 +0530

    add img navbar

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -82,7 +82,7 @@ const Start=()=> {
         {
           localStorage.getItem('token')?(
                 <div>
-                <button  className=' '>Welcome back, {user?.name}</button>
+                <button  className=' '>{user?.name}<img className='h-[30px] w-[30px] ml-2 rounded-full' src={user?.img}></img></button>
                 </div>                
             ):(
                 <div className='flex gap-4'>
